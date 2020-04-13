76e19129514c6da5b0722e839ba27ea65679e9cb
Matheus Lessa <matheus@fastmail.com>
2020-04-13 11:05:38 -0300

    it works

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # copycat
+
+A simple crossplatform clipboard cli interface.
+
+Pipe into it to copy. Pipe from it to paste.
+
+```
+$ echo "copy this!" | copycat
+```
+
+```
+$ copycat
+> copy this!
+```
+
+```
+$ copycat | grep .
+> copy this!
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io::{stdin, Read};
+use clipboard2::{Clipboard, SystemClipboard};
+use atty::Stream;
+
 fn main() {
-    println!("Hello, world!");
+    let clipboard = SystemClipboard::new().unwrap();
+
+    if atty::isnt(Stream::Stdin) {
+        let mut text = String::new();
+        stdin().lock().read_to_string(&mut text).unwrap();
+        if text.ends_with('\n') {
+            text.pop();
+        }
+        clipboard.set_string_contents(text).unwrap();
+    } else {
+        let text = clipboard.get_string_contents().unwrap();
+        if atty::is(Stream::Stdout) {
+            if text.ends_with('\n') {
+                print!("{}", text);
+            } else {
+                println!("{}", text);
+            }
+        } else {
+            print!("{}", text);
+        }
+    }
 }
